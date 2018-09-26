904e788132b0cb1f868c27639e2dfb3fbcab88ac
geesondog <32091537+geesondog@users.noreply.github.com>
2018-09-26 15:49:28 +0800

    微信支支付补上红包Redpack类

--- extend/wxpayAPI/lib/WxPay.Data.php
+++ extend/wxpayAPI/lib/WxPay.Data.php
@@ -2983,3 +2983,99 @@ class WxPayBizPayUrl extends WxPayDataBase
 		return array_key_exists('product_id', $this->values);
 	}
 }
+/**
+ * 现金红包类
+ * 参数以及使用方法请查看发红包函数
+ * @author Geeson <314835050@qq.com>
+ * Class Redpack
+ */
+class Redpack extends WxPayDataBase
+{
+
+    function trimString($value)
+    {
+        $ret = null;
+        if (null != $value) {
+            $ret = $value;
+            if (strlen($ret) == 0) {
+                $ret = null;
+            }
+        }
+        return $ret;
+    }
+
+    public function setParments($parameter, $parameterValue)
+    {
+        $this->values[$this->trimString($parameter)] = $this->trimString($parameterValue);
+    }
+
+    public function createRedpackXml()
+    {
+        $this->values['wxappid'] = WxPayConfig::$APPID;
+        $this->values['mch_id'] = WxPayConfig::$MCHID;
+        $this->values['sign'] = $this->MakeSign();
+        return $this->ToXml($this->values);
+    }
+
+    /**
+     * 以post方式提交xml到对应的接口url
+     *
+     * @param string $xml  需要post的xml数据
+     * @param string $url  url
+     * @param bool $useCert 是否需要证书，默认不需要
+     * @param int $second   url执行超时时间，默认30s
+     * @throws WxPayException
+     */
+    public function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+
+        //如果有配置代理这里就设置代理
+        if(WxPayConfig::$CURL_PROXY_HOST != "0.0.0.0"
+            && WxPayConfig::$CURL_PROXY_PORT != 0){
+            curl_setopt($ch,CURLOPT_PROXY, WxPayConfig::$CURL_PROXY_HOST);
+            curl_setopt($ch,CURLOPT_PROXYPORT, WxPayConfig::$CURL_PROXY_PORT);
+        }
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        if($useCert == true){
+            //设置证书
+            //使用证书：cert 与 key 分别属于两个.pem文件
+            curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLCERT, WxPayConfig::$SSLCERT_PATH);
+            curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLKEY, WxPayConfig::$SSLKEY_PATH);
+        }
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+            throw new WxPayException("curl出错，错误码:$error");
+        }
+    }
+
+    public function xmlToArray($xml)
+    {
+        //将XML转为array
+        $array_data = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        return $array_data;
+    }
+
+
+}
